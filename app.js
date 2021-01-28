474c87194ef831a7e47db05613fcbd04d5cf36b3
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-01-28 15:12:05 -0500

    add comments instructing what the Rest API routes are intended to do.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/greetings', () => {
-    
-})
+app.get('/greetings', (req, res) => {
+    res.json({greeting: 'Hello World!'})
+});
+
+// Send a GET request to READ(view) a quote
+// Send a GET request to READ(view) a list of quotes
+// Send a GET request to READ(view) a random quote
+// Send a POST request to CREATE a new quote
+// Send a PUT request to UPDATE(edit) a quote
+// Send a DELETE request to remove a quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
