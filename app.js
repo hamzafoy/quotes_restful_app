7ad3f046acaf1b2bb31db370720da99b3ecea63f
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-01-30 00:53:36 -0500

    implement try/catch blocks and add a POST route to add quotes.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const records = require('./records');
 
+app.use(express.json());
+
 app.get('/greetings', (req, res) => {
     res.json({greeting: 'Hello World!'})
 });
@@ -9,19 +11,40 @@ app.get('/greetings', (req, res) => {
 // Send a GET request to /quotes/:id to READ(view) a quote
 
 app.get('/quotes/:id', async (req, res) => {
+
+    try {
     const quote = await records.getQuote(req.params.id);
     res.json(quote);
+    } catch(err) {
+        res.json({message: err.message});
+    };
 });
 
 // Send a GET request to /quotes to READ(view) a list of quotes
 
 app.get('/quotes', async (req, res) => {
+    try {
     const quotes = await records.getQuotes();
     res.json(quotes);
+    } catch(err) {
+        res.json({message: err.message});
+    };
 });
 
 // Send a GET request to /quotes/quote/random to READ(view) a random quote
 // Send a POST request to /quotes to CREATE a new quote
+
+app.post('/quotes', async (req, res) => {
+    try {
+        const quote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author,
+        });
+        res.json(quote);
+    } catch(err) {
+        res.json({message: err.message});
+    };
+});
 // Send a PUT request to /quotes/:id to UPDATE(edit) a quote
 // Send a DELETE request to /quotes/:id to remove a quote
 
