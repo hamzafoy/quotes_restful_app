c9cc0ca78cbb7ef4f92d7ee1e4b67d573f30b059
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-01-30 01:56:17 -0500

    add appropriate status codes for the GET and POST routes created so far and create appropriate error messages.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const records = require('./records');
@@ -14,7 +15,11 @@ app.get('/quotes/:id', async (req, res) => {
 
     try {
     const quote = await records.getQuote(req.params.id);
-    res.json(quote);
+    if (quote) {
+        res.json(quote);
+    } else {
+        res.status(404).json({message: 'The quote you requested is not found.'})
+    }
     } catch(err) {
         res.json({message: err.message});
     };
@@ -36,12 +41,17 @@ app.get('/quotes', async (req, res) => {
 
 app.post('/quotes', async (req, res) => {
     try {
-        const quote = await records.createQuote({
-            quote: req.body.quote,
-            author: req.body.author,
-        });
-        res.json(quote);
+        if (req.body.author && req.body.quote) {
+            const quote = await records.createQuote({
+                quote: req.body.quote,
+                author: req.body.author,
+            });
+            res.status(201).json(quote);
+        } else {
+            res.status(400).json({message: "Your submission is incomplete or contains an error."})
+        }
     } catch(err) {
+        res.status (500);
         res.json({message: err.message});
     };
 });
