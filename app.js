75f9f6cf093e2c2f5dc49318be3f056758b6dd03
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-01-31 21:17:21 -0500

    add global error handler that returns error information in .json format.

--- app.js
+++ app.js
@@ -89,4 +89,21 @@ app.delete('/quotes/:id', async (req, res) => {
     }
 });
 
+// Middleware for handling errors globally
+
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    })
+})
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
